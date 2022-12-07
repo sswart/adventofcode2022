25bd692fc6ed3c58851ee341cb66e94e2a98e480
Jesse Swart <sswart@users.noreply.github.com>
2022-12-07 11:02:10 +0100

    Day6

---
+++ dotnet/Day6/PacketFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet.Day6
+{
+    internal static class PacketFinder
+    {
+        public static int FindPacketStart(string input)
+        {
+            return FindMarker(input, 4);
+        }
+
+        public static int FindMessageStart(string input)
+        {
+            return FindMarker(input, 14);
+        }
+        private static int FindMarker(string input, int markerLength)
+        {
+            for (int i = 0; i < input.Length - markerLength; i++)
+            {
+                var potentialPacket = input.Substring(i, markerLength);
+                if (potentialPacket.Distinct().Count() == markerLength)
+                {
+                    return i + markerLength;
+                }
+            }
+            throw new ArgumentException("Input does not contain packet");
+        }
+    }
+}
---
+++ dotnet/Day6/PacketFinderTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dotnet.Day6
+{
+    public class PacketFinderTest
+    {
+        [Fact]
+        public void TestPacketStart()
+        {
+            PacketFinder.FindPacketStart("mjqjpqmgbljsphdztnvjfqwrcgsmlb").Should().Be(7);
+            PacketFinder.FindPacketStart("bvwbjplbgvbhsrlpgdmjqwftvncz").Should().Be(5);
+            PacketFinder.FindPacketStart("nppdvjthqldpwncqszvftbrmjlhg").Should().Be(6);
+            PacketFinder.FindPacketStart("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").Should().Be(10);
+            PacketFinder.FindPacketStart("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").Should().Be(11);
+        }
+
+        [Fact]
+        public void TestMessageStart()
+        {
+            PacketFinder.FindMessageStart("mjqjpqmgbljsphdztnvjfqwrcgsmlb").Should().Be(19);
+            PacketFinder.FindMessageStart("bvwbjplbgvbhsrlpgdmjqwftvncz").Should().Be(23);
+            PacketFinder.FindMessageStart("nppdvjthqldpwncqszvftbrmjlhg").Should().Be(23);
+            PacketFinder.FindMessageStart("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").Should().Be(29);
+            PacketFinder.FindMessageStart("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").Should().Be(26);
+        }
+    }
+}
